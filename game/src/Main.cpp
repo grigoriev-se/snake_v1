e39d87975e863a548a9e5d225b203f88fed582b4
Nikita Grigoriev <nikita.grigoriev@knightec.se>
2022-11-08 20:31:56 +0100

    new board update halfway done

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -11,8 +11,8 @@ using namespace std;
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
-int ROW = 10;
-int COL = 10;
+int ROW = 5;
+int COL = 7;
 
 class apple {
 public:
@@ -30,9 +30,9 @@ public:
 
 	void move(int xdir, int ydir) {
 		int snake_size = x.size();
-		while (snake_size-1 != 0) {
-			x[snake_size-1] = x[snake_size-2];
-			y[snake_size-1] = y[snake_size-2];
+		while (snake_size - 1 != 0) {
+			x[snake_size - 1] = x[snake_size - 2];
+			y[snake_size - 1] = y[snake_size - 2];
 			snake_size -= 1;
 		}
 		x[0] += xdir;
@@ -46,43 +46,64 @@ public:
 	}
 };
 
+class board {
 
-void generate_board(snake snake, apple apple) {
-	// Generate board
-	int tail_counter = 0;
+public:
+	vector<string> generate_new() {
+		int AREA = ROW * COL;
+		std::vector<string> board(AREA, " ");
+		std::fill(board.begin(), board.begin() + COL, "#");
+		std::fill(board.end() - COL, board.end(), "#");
+		for (int jj = 1; jj < ROW - 1; ++jj) {
+			board[COL * jj] = "#";
+			board[COL * (jj + 1) - 1] = "#";
+		}
+		return board;
+	}
 
-	for (int rows = 0; rows < (ROW); ++rows) {
-		for (int cols = 0; cols < (COL); ++cols) {
-			if (snake.x[0] == cols && snake.y[0] == rows) {
-				cout << "O";
-			}
-			//// This part incorrectly generates the snake tail
-			// FIX THIS - almost works
-			//if (snake.x[tail_counter] == cols && snake.y[tail_counter] == rows) {
-			//	cout << "S";
-			//	tail_counter += 1;
-			//}
-
-			else if (rows == 0 || rows == (ROW - 1)) {
-				cout << "#";
-			}
-			else if (cols == 0 || cols == (COL - 1)) {
-				cout << "#";
-			}
-			// Put either " " or snake
-			else if (apple.x == cols && apple.y == rows) {
-				cout << "d";
-			}
-			else
-			{
-				cout << " ";
+	void plot_board(vector<string> board_vector, apple apple) {
+		cout << "plot_board:\n";
+		// Update the apple:
+		cout << "Apple coordinates (x,y): " << apple.x << " " << apple.y << ". \n";
+		board_vector[apple.y * (COL -1) + apple.y + apple.x] = "Q";
+		for (int x = 0; x < board_vector.size(); ++x) {
+			cout << board_vector[x];
+			if ((x + 1) % COL == 0) {
+				cout << "\n";
 			}
 		}
-		cout << "\n";
 	}
-	cout << "\nPress UP/DOWN/LEFT/RIGHT keys: ";
 
-}
+	vector<string> update_board(vector<string> board_vector, snake snake, apple apple) {
+		// Update the snake and tail positions:
+
+		cout << "FOR LOOP:\n";
+		for (int rows = 0; rows < (ROW); ++rows) {
+			for (int cols = 0; cols < (COL); ++cols) {
+				if (snake.x[0] == cols && snake.y[0] == rows) {
+					cout << "O";
+				}
+				else if (rows == 0 || rows == (ROW - 1)) {
+					cout << "#";
+				}
+				else if (cols == 0 || cols == (COL - 1)) {
+					cout << "#";
+				}
+				// Put either " " or apple
+				else if (apple.x == cols && apple.y == rows) {
+					cout << "d";
+				}
+				else
+				{
+					cout << " ";
+				}
+			}
+			cout << "\n";
+		}
+		cout << "\nPress UP/DOWN/LEFT/RIGHT keys: ";
+		return board_vector;
+	}
+};
 
 snake check_boundaries(snake snake) {
 	int snake_size = snake.x.size();
@@ -120,27 +141,34 @@ int main() {
 
 	snake snake;
 	apple apple;
+	board board;
 
 	srand((unsigned)time(NULL));
 	snake.x.push_back(1 + (rand() % (COL - 2)));
 	snake.y.push_back(1 + (rand() % (ROW - 2)));
 	apple.generate_pos();
 
+	vector<string> board_vector = board.generate_new();
+	board_vector = board.update_board(board_vector, snake, apple);
+
 	while (run_game(a) == true) {
 		if (apple.x == snake.x[0] && apple.y == snake.y[0]) {
-			apple.generate_pos(); // Generates a new position
 			snake.generate_tail(apple); // Puts an apple in the tail
+			apple.generate_pos(); // Generates a new position
+		}
+		for (int ii = 0; ii < snake.x.size(); ++ii) {
+			cout << "Snake tail (x,y): " << snake.x[ii] << "," << snake.y[ii] << "\n";
+
 		}
 		//for (int ii = 0; ii < tail.size; ++ii) {
 		//	tail.move(snake, ii);
 		//}
 
-		cout << "head_x: " << snake.x[0] << "\n";
-		cout << "head_y: " << snake.y[0] << "\n";
+		//cout << "head (x,y): " << snake.x[0] << "," << snake.y[0] << "\n";
 		cout << "Snake size: " << snake.x.size() << "\n";
 
-		generate_board(snake, apple);
-
+		board_vector = board.update_board(board_vector, snake, apple);
+		board.plot_board(board_vector, apple);
 		key = _getch();
 		value = key;
 
